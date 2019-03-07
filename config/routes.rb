6bdd85c080cac7bb6dc6fd499b70a50beb91f50a
TheRubyJailCell <ruby666aki@gmail.com>
2019-03-07 13:32:38 +0100

    root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :constellations
-  root to:"constellations"
+  root to: "constellations#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
