5eddccc5e7c2c79bab60ad28342506abc394c622
V MARKUS <ruby666aki@gmail.com>
2022-01-12 11:53:16 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  #test
+  config.secret_key_base = SecureRandom.hex(64) 
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
